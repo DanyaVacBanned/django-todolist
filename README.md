4a6a10827016040169a33ffdbbddeec1eb36da87
@danil_matytsyn <sanych120@mail.ru>
2023-05-11 16:41:21 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+<h1> DJANGO TODOLIST </h1>
+
+<h2>
+    <p> This is my try to modify TODO LIST project by <a href='https://github.com/divanov11/Django-To-Do-list-with-user-authentication'>this dude</a>
+</h2>
+
+<h2>
+    <p> TODO xd </p>
+</h2>
+<table>
+    <tr>
+        <td><p>Make some css</p></td>
+        <td><p>Add pretty animations via javascript</p></td>
+        <td><p>Integrate telegram api for notifications from telegram bot</p></td>
+    </tr>
+</table>
+
+<p>
+    This is not a complete list of my plans on this project. Over time, I will come up with more features.
+</p>
+
+<h4>
+    I dont want deploy this on server. Maybe in future this project will be see the world.
+</h4>
---
+++ todolist/views.py
@@ -0,0 +1,91 @@
+from typing import Any, Dict
+from django.forms.models import BaseModelForm
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import redirect
+
+from django.urls import reverse_lazy
+
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import login
+
+from .models import Task
+
+
+class RegisterPage(FormView):
+    template_name = 'todolist/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+    def form_valid(self, form: Any):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+    def get(self, *args: str, **kwargs: Any):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
+class CustomLoginView(LoginView):
+    template_name = 'todolist/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+    def get_success_url(self) -> str:
+        return reverse_lazy('tasks')
+    
+
+
+class CreateTask(LoginRequiredMixin,CreateView):
+    model = Task
+    template_name = 'todolist/task_form.html'
+    context_object_name = 'createTask'
+    fields = ['title', 'description', 'complete']
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form: BaseModelForm):
+        form.instance.user = self.request.user
+        return super(CreateTask, self).form_valid(form)
+
+
+class TaskList(LoginRequiredMixin, ListView):
+    model = Task
+    context_object_name = 'tasks'
+    template_name = 'todolist/task_list.html'
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete=False).count()
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__startswith=search_input)
+        context['search_input'] = search_input
+        return context
+    
+
+
+class TaskDetail(LoginRequiredMixin,DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'todolist/task_details.html'
+
+
+class UpdateTask(LoginRequiredMixin,UpdateView):
+    model = Task
+    
+    context_object_name = 'updateTask'
+    fields = ['title', 'description', 'complete']
+    success_url = reverse_lazy('tasks')
+
+class DeleteTask(LoginRequiredMixin,DeleteView):
+    model = Task
+    success_url = reverse_lazy('tasks')
+    context_object_name = 'task'
+
+
